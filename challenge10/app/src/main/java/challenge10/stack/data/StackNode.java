fd48cc8dcbf43bd535e7f44ca01d869621f9a4f2
roaa1998 <roaabhailat4@gmail.com>
2022-03-16 01:12:34 +0300

    stack and queue

---
+++ challenge10/app/src/main/java/challenge10/stack/data/StackNode.java
@@ -0,0 +1,34 @@
+package challenge10.stack.data;
+
+public class StackNode {
+    private String value;
+    private StackNode next;
+
+    public StackNode(String value){
+        this.value=value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setNext(StackNode next) {
+        this.next = next;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public StackNode getNext() {
+        return next;
+    }
+
+    @Override
+    public String toString() {
+        return "StackNode{" +
+                "value='" + value + '\'' +
+                ", next=" + next +
+                '}';
+    }
+}
